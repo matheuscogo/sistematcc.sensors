56e6631f1b780c60472ce4e0eff832317839de29
matheuscogo <matheus.2018011821@aluno.iffar.edu.br>
2022-04-26 02:04:05 -0300

    Descrição:Alterando a regra para simulação do alimentador

--- src/app.py
+++ src/app.py
@@ -7,29 +7,45 @@ import random
 import time
 import datetime
 
+#Botão do sensor PIR
 sensorPIR=29
-porta1Fechada=31
-porta1Aberta=33
-porta2Fechada=35
-porta2Aberta=37
 
-porta1Fechando=32
-porta1Abrindo=36
-porta2Fechando=38
-porta2Abrindo=40
+#Botão do portão principal(curso aberto)
+cursoAbertura=31 #porta1Aberta
+
+#Botão do portão principal(curso fechado)
+cursoFechamento=33 #porta1Fechada
+
+#Botão do portão separador(curso aberto)
+cursoSepadorAbertura=35 #porta2Aberta
+
+#Botão do portão separador(curso fechado)
+cursoSepadorFechamento=37 #porta2Fechada
+
+#Portão aberto(led verde)
+portaoAberto=36
+
+#Portão fechado(led vermelho)
+portaoFechado=32
+
+#Portão do separador aberto(led verde)
+portaoSeparadorAberto=40
+
+#Portão do separador fechada(led vermelho)
+portaoSeparadorFechado=38
 
 GPIO.setmode(GPIO.BOARD) #Définit le mode de numérotation (Board)
 GPIO.setwarnings(False) #On désactive les messages d'alerte
 GPIO.setup(sensorPIR,GPIO.IN)
 
-GPIO.setup(porta1Fechada,GPIO.OUT)
-GPIO.setup(porta2Fechada,GPIO.OUT)
-GPIO.setup(porta1Aberta,GPIO.OUT)
-GPIO.setup(porta2Aberta,GPIO.OUT)
-GPIO.setup(porta1Fechando,GPIO.OUT)
-GPIO.setup(porta1Abrindo,GPIO.OUT)
-GPIO.setup(porta2Fechando,GPIO.OUT)
-GPIO.setup(porta2Abrindo,GPIO.OUT)
+GPIO.setup(cursoAbertura,GPIO.OUT)
+GPIO.setup(cursoFechamento,GPIO.OUT)
+GPIO.setup(cursoSepadorAbertura,GPIO.OUT)
+GPIO.setup(cursoSepadorFechamento,GPIO.OUT)
+GPIO.setup(portaoAberto,GPIO.OUT)
+GPIO.setup(portaoFechado,GPIO.OUT)
+GPIO.setup(portaoSeparadorAberto,GPIO.OUT)
+GPIO.setup(portaoSeparadorFechado,GPIO.OUT)
 
 def lerTag():  # retorna brinco aleatoriamente 
     tag=["123456789","987654321"]
@@ -38,86 +54,83 @@ def lerTag():  # retorna brinco aleatoriamente
 
 def start():
     fixarPIR=0 # simular sensor PIR
-    P1=1 # porta 1 aberta (0 ou 1)
-    P2=1 # porta 2 aberta (0 ou 1)
+    portao=1 # porta 1 aberta (0 ou 1)
+    Separador=1 # porta 2 aberta (0 ou 1)
     brincoLido=0 # animal na maquina e brinco lido (1), sai da maquina (0) 
     
     print('Aperte botão sensorPIR (Ou saia com Ctrl + c): ')
 
     horaVeficada = datetime.datetime.today().hour
-    #On va faire une boucle infinie pour lire en boucle
+    GPIO.output(portaoAberto,1)
+
     while True:
-
 # ==== So teste dos leds porta 2 ========
-        if(GPIO.input(porta2Fechada) == 1):
-            print("2fechada")
-            GPIO.output(porta2Fechando,1)
+        if(GPIO.input(cursoSepadorFechamento) == 1):
+            GPIO.output(portaoSeparadorFechado,1)
+            # print("Portão separador fechando...")
         else:
-            GPIO.output(porta2Fechando,0)
+            GPIO.output(portaoSeparadorFechado,0)
         
-        
-        if(GPIO.input(porta2Aberta) == 1):
-            print("2aberta")
-            GPIO.output(porta2Abrindo,1)
+        if(GPIO.input(cursoSepadorAbertura) == 1):
+            # print("Portão separador fechando...")
+            GPIO.output(portaoSeparadorAberto,1)
         else:
-            GPIO.output(porta2Abrindo,0)
-
-        data = datetime.datetime.today().hour - horaVeficada
+            GPIO.output(portaoSeparadorAberto,0)
         
         if datetime.datetime.today().hour - horaVeficada  > 24:
             confinamentos.verifyDaysToOpen()
             horaVeficada = datetime.datetime.today().hour
-    # ========================================    
 
-        if(GPIO.input(sensorPIR) == 1 or fixarPIR == 1 ):
+        if(GPIO.input(sensorPIR) == 1 or fixarPIR == 1):
             if(fixarPIR == 0 and GPIO.input(sensorPIR) == 1):
                 time.sleep(0.5)
-
-                # simular que PIR esta detectando presença
+                # Simular que PIR esta detectando presença
                 fixarPIR = 1
+
             if(fixarPIR == 1 and GPIO.input(sensorPIR) == 1):
                 time.sleep(0.5)
+                # Simular que PIR não detectou presença
                 fixarPIR = 0
         
-            print("...")
-        
-            # se porta 1 aberta então fechar
-            if(P1==1):
-                GPIO.output(porta1Fechando,1)
-                print("1fechando")
+            # Se portão estiver aberto, então fechar
+            if(portao==1):
+                GPIO.output(portaoFechado,0)
+                GPIO.output(portaoAberto,0)
+                print("Fechando portão...")
             
-            # fim de curso porta fechada
-            if(GPIO.input(porta1Fechada) == 1):
-                print("1fechada")
-                GPIO.output(porta1Fechando,0)
-                P1=0; # porta 1 fechada
+                # Fim de curso portão fechado
+                if(GPIO.input(cursoAbertura) == 1):
+                    print("Portão fechado")
+                    GPIO.output(portaoAberto,0)
+                    GPIO.output(portaoFechado,1)
+                    portao=0; # porta 1 fechada
 
-            # enquanto brinco não lido ficar tentando ler
-            if(not brincoLido):
-                brinco=lerTag()
+                    # enquanto brinco não lido ficar tentando ler
+                    if(not brincoLido):
+                        brinco=lerTag()
                 
-                matriz = matrizes.getMatrizByRfid(brinco)
-                confinamento = confinamentos.getConfinamentoByMatriz(matriz['id'])
+                        matriz = matrizes.getMatrizByRfid(brinco)
+                        confinamento = confinamentos.getConfinamentoByMatriz(matriz['id'])
                 
-                quantidade = confinamentos.getQuantityForMatriz(matriz['id'])
+                        quantidade = confinamentos.getQuantityForMatriz(matriz['id'])
                 
-                brincoLido=1    
-                print ('Tag {}!'.format(brinco))
-
-        if(fixarPIR == 0 ):
-
+                        brincoLido=1    
+                        print ('Matriz {} identificada'.format(brinco))
+                    
+        if(fixarPIR == 0):
             brincoLido=0
 
-            # se porta 1 fechada então abrir
-            if(P1==0):
-                GPIO.output(porta1Abrindo,1)
-                print("1abrindo")
+            # Se portão fechado, então abrir
+            if(portao==0):
+                GPIO.output(portaoAberto,0)
+                GPIO.output(portaoFechado,0)
+                print("Abrindo portão...")
 
-            # fim de curso porta fechada
-            if(GPIO.input(porta1Aberta) == 1):
-                GPIO.output(porta1Abrindo,0)
-                print("1aberta")
-                P1=1; # porta 1 aberta
+                # Fim de curso e portão fechada
+                if(GPIO.input(cursoFechamento) == 1):
+                    GPIO.output(portaoAberto,1)
+                    print("Portão aberto")
+                    portao=1 # Portão aberto
 
 
 if __name__ == "__main__":
