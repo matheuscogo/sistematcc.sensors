d4fa3e8d506d3f67b52ebb86c10c6381fb52b60d
matheuscogo <matheus.2018011821@aluno.iffar.edu.br>
2022-11-19 03:44:42 -0300

    ajustando sensores;

--- src/ext/site/model/Dia.py
+++ src/ext/site/model/Dia.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, ForeignKey
 from ext.db import db
+
 
 class Dia(db):
     __tablename__ = "dias"
     # __table_args__ = {"schema":"public"}
     id = Column("id", Integer, primary_key=True, autoincrement=True)
-    planoId = Column("plano_id", Integer,  ForeignKey("sistemaTCC.planos.id"))
+    planoId = Column("plano_id", Integer,  ForeignKey("planos.id"))
     dia = Column("dia", Integer)
     quantidade = Column("quantidade", Integer)
